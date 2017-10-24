5b7557ea085e3249b0ba25ce2e32c90b4d1a1f01
Malikpako <x0ulhacker786@gmail.com>
2017-10-24 15:02:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+        <?php
+        $Days = array("sunday","monday","tuesday","wednesday","thursday","friday","saturday");{
+         for($i = 0; $i< count ($Days);$i++){
+             echo $Days[$i];
+             echo"<br>";
+         }
+            
+            
+            
+            
+        }
+      
+  
